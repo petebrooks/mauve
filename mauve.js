3e20c5feeffa90c3fb3bb17e080e55dcaedff7cb
Pete Brooks <pete.brooks@tablexi.com>
2015-09-20 18:57:14 -0500

    add readme and gitignore

--- mauve.js
+++ mauve.js
@@ -3,70 +3,42 @@ Movies = new Mongo.Collection("movies");
 if (Meteor.isClient) {
   // // counter starts at 0
   // Session.setDefault('counter', 0);
+  $(".colorField").hide();
 
-  function getHexColor(e) {
-    var $el = $(e.target)
-    var h = (e.pageX / $el.width()) * 360;
-    var s = e.pageY / $el.height();
-    var l = 0.5;
-    return tinycolor({ h: h, s: s, l: l }).toHexString();
-  }
-
-  Template.body.helpers({
+  Template.home.helpers({
     movies: function() {
-      return Movies.find({});
+      var searchTerm = Session.get("searchTerm");
+      if (searchTerm) {
+        return Movies.find({title: new RegExp(searchTerm) });
+      } else {
+        return Movies.find({});
+      };
     }
   });
 
-  Template.mov.helpers({
-    formatDate: function() {
-      return moment(this.createdAt).format('MM/DD/YYYY');
-    }
-  });
-
-  Template.mov.onRendered( function() {
-    var color = tinycolor(this.data.hexColor);
-    var $el = $(this.firstNode);
-    var backgroundColor = color.toHexString();
-    var textColor = tinycolor.mostReadable(color, ["#fff", "#000"]).toHexString();
-    $el.css({
-      "background-color": backgroundColor,
-      "color": textColor
-    });
-  });
-
-  Template.body.events({
-    "mousemove": function(e) {
-      if(e.target === $(".color_r")[0] && e.buttons > 0) {
-        var hex = getHexColor(e);
-        $(".color_r").css("background-color", hex);
-        $("input[name=hexColor]").val(hex);
-      }
-    },
-
-    "submit .new-movie": function(e) {
+  Template.mainLayout.events({
+    "click button": function(e) {
       e.preventDefault();
 
-      var title = e.target.title.value.trim();
-      var hexColor = e.target.hexColor.value.trim() || "#ffffff";
+      var $el = $(e.target),
+          route = $el.attr("href"),
+          show = $el.attr("show"),
+          form = $el.attr("form");
 
-      if (title) {
-        Movies.insert({
-          title: title,
-          hexColor: hexColor,
-          createdAt: new Date()
-        });
-
-        e.target.title.value = "";
+      if (route) {
+        FlowRouter.go(route);
+      } else if (show) {
+        $(show).show();
+        $el.hide();
+      } else if (form) {
+        $(form).trigger("submit");
       };
     },
 
-    "keypress .new-movie": function(e) {
-      var $el = $(e.target);
-      var complete = $el.val().trim();
-      complete ? $el.removeClass("error") : $el.addClass("error");
+    "keyup #search": function(e) {
+      Session.set("searchTerm", e.target.value);
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
